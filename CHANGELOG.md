929b372272be7ede977d30c6cccc0cafcc054afa
Yutsuro <90205823+Yutsuro@users.noreply.github.com>
2025-08-25 02:35:26 +0900

    v0.1.2

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# Changelog
+
+## [Unreleased]
+
+## [0.1.2] - 2025-08-25
+### Changed
+* Replace `japanize-matplotlib` to `matplotlib-fontja`.
+
+## [0.1.1] - 2024-06-15
+### Added
+* Add `japanize-matplotlib` package.
+
+## [0.1.0] - 2024-04-14
+First Publish.
--- tisc/tisc.py
+++ tisc/tisc.py
@@ -2,12 +2,13 @@ import os
 import copy
 import torch
 import random
-import japanize_matplotlib
 
 import numpy as np
 import torch.nn as nn
 import torch.optim as optim
+
 import matplotlib.pyplot as plt
+import matplotlib_fontja
 import seaborn as sns; sns.set_theme(font='IPAexGothic')
 
 from datetime import datetime
